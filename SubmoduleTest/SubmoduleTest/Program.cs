9f920ecbcd8ed87f8b1c11d4902ed97463c9479b
valyushitskys <valyushitskys@USTN-DEV06>
2016-11-02 08:32:16 -0500

    Add submodule.

--- SubmoduleTest/SubmoduleTest/Program.cs
+++ SubmoduleTest/SubmoduleTest/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Submodule1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,9 @@ namespace SubmoduleTest
     {
         static void Main(string[] args)
         {
+            var s = new Submodule();
+            s.WriteLine();
+            Console.ReadKey();
         }
     }
 }
